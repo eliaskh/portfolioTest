a1f64c34bdeaba97fd33c6e729215b8f435c7c2a
elias <eliaskhs@hotmail.com>
2022-05-12 22:01:43 +0300

    second

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,37 +1,35 @@
-import React from "react";
-import Layout from "../components/_App/layout";
-import SEO from "../components/_App/seo";
-import Navbar from "../components/_App/Navbar";
-import MainBanner from "../components/ITStartup/MainBanner";
-import Features from "../components/ITStartup/Features";
-import ServicesArea from "../components/ITStartup/ServicesArea";
-import OurFeatures from "../components/ITStartup/OurFeatures";
-import Team from "../components/Common/Team";
-import FunFactsArea from "../components/Common/FunFactsArea";
-import RecentWorks from "../components/Common/RecentWorks";
-import PricingStyleOne from "../components/PricingPlans/PricingStyleOne";
-import FeedbackStyleTwo from "../components/Common/FeedbackStyleTwo";
-import Partner from "../components/Common/Partner";
-import BlogPost from "../components/Common/BlogPost";
-import Footer from "../components/_App/Footer";
+import React from "react"
+import Layout from "../components/_App/layout"
+import SEO from "../components/_App/seo"
+import NavbarStyleFour from "../components/_App/NavbarStyleFour"
+import MainBanner from "../components/BigdataAnalytics/MainBanner"
+import WhatWeDo from "../components/BigdataAnalytics/WhatWeDo"
+import DiscoverArea from "../components/BigdataAnalytics/DiscoverArea"
+import Services from "../components/BigdataAnalytics/Services"
+import TeamStyleTwo from "../components/Common/TeamStyleTwo"
+import BigdataFunFacts from "../components/BigdataAnalytics/BigdataFunFacts"
+import Feedback from "../components/BigdataAnalytics/Feedback"
+import IndustriesWeServe from "../components/BigdataAnalytics/IndustriesWeServe"
+import BlogPost from "../components/BigdataAnalytics/BlogPost"
+import NewsletterStyleTwo from "../components/Common/NewsletterStyleTwo"
+import Footer from "../components/_App/Footer"
 
-const IndexPage = () => (
-    <Layout>
-        <SEO title="Home" />
-        <Navbar />
-        <MainBanner />
-        <Features />
-        <ServicesArea />
-        <OurFeatures />
-        <Team />
-        <FunFactsArea />
-        <RecentWorks />
-        <PricingStyleOne />
-        <FeedbackStyleTwo />
-        <Partner />
-        <BlogPost />
-        <Footer />
-    </Layout>
+const BigdataAnalytics = () => (
+  <Layout>
+    <SEO title="Home" />
+    <NavbarStyleFour />
+    <MainBanner />
+    <WhatWeDo />
+    <DiscoverArea />
+    <Services />
+    <TeamStyleTwo />
+    <BigdataFunFacts />
+    <Feedback />
+    <IndustriesWeServe />
+    <BlogPost />
+    <NewsletterStyleTwo />
+    <Footer />
+  </Layout>
 )
 
-export default IndexPage;
+export default BigdataAnalytics
