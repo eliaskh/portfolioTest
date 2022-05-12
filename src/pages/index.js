3f1e6946e20eb91859c1f9ce425fa7c4eaa79f59
elias <eliaskhs@hotmail.com>
2022-05-12 23:21:25 +0300

    elias

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,7 +20,7 @@ const BigdataAnalytics = () => (
     <NavbarStyleFour />
     <MainBanner />
     <WhatWeDo />
-    <DiscoverArea />
+    // <DiscoverArea />
     <Services />
     <TeamStyleTwo />
     <BigdataFunFacts />
